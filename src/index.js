d76284f289d1bdb60cf1d585d2371ce95b3a68e3
emceekoenig <129642888+emceekoenig@users.noreply.github.com>
2024-06-23 11:21:24 -0500

    Improved the User Experience (UX)
    -added element for prompt/topic suggestions
    -added animation for "generating..." message so users are indicated of when the AI is processing their prompt
    -implemented dark mode styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+let poemElement = document.querySelector("#poem");
+
 function displayPoem(response) {
-  const poem = response.data.answer;
+  poemElement.classList.remove("animate-pulse");
+  const poemResult = response.data.answer;
   new Typewriter("#poem", {
-    strings: poem,
+    strings: poemResult,
     autoStart: true,
     cursor: "",
     delay: 50,
@@ -16,18 +19,20 @@ function generatePoem(event) {
   // display the generated poem
 
   let instructionsInput = document.querySelector("#user-instructions");
-
   let apiKey = "50f181fb5atco86d3e4e13e20785aa08";
   let prompt = `User instructions: generate a unique haiku about ${instructionsInput.value}`;
   let context =
-    "You are a haiku poem expert. Your mission is to generate a haiku in basic HTML. Separate each line with a <br />. Do not include a title. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem";
+    "You are a haiku poem expert. Your mission is to generate a haiku in basic HTML. Separate each line with a <br />. Do not include a title. Make sure to follow the user instructions. At the end of the poem, sign the poem with 'SheCodes AI' wrapped inside a <p> element.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem...");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `Generating a haiku about
+    <em>${instructionsInput.value}...</em>
+`;
 
-  axios.get(apiUrl).then(displayPoem);
+  setTimeout(() => {
+    axios.get(apiUrl).then(displayPoem);
+  }, 3000);
 }
 
 let formElement = document.querySelector("#poem-generator-form");
